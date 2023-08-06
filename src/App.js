95423eb00a267c7c749b2aeffa5baac9737415f8
Ahsan855 <a.r.ahsanhabib9869@gmail.com>
2023-08-06 21:06:08 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <h1 className="bg-green-500 text-red-500 text-4xl text-center p-11 font-extrabold">
+        This is Heading text
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Home = () => {
+  return (
+    <div>
+      <h1>This is H1 tag</h1>
+    </div>
+  );
+};
+export default Home;
